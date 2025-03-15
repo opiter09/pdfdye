b06a55a23d139adcb71734eff879f0ba3baa29a6
opiter09 <92452568+opiter09@users.noreply.github.com>
2025-03-14 21:58:56 -0400

    actually tells you when it's done

--- exec.py
+++ exec.py
@@ -9,18 +9,22 @@ layout = [
     [ psg.Text("Green:"), psg.DropDown(list(range(256)), default_value = "0", key = "green") ],
     [ psg.Text("Blue:"), psg.DropDown(list(range(256)), default_value = "0", key = "blue") ],
     [ psg.Text("Output PDF:"), psg.FileSaveAs(key = "output", file_types = [("PDF Files", ".pdf")]) ],
-    [ psg.Submit() ]
+    [ psg.Button("Submit", key = "submit") ]
 ]
 
 window = psg.Window("", layout, grab_anywhere = True, font = "-size 12")
-event, values = window.read()
-window.close()
 
-if (values["red"] != None):
-    redHex = str(hex(int(values["red"])))[2:].upper().zfill(2)
-    greenHex = str(hex(int(values["green"])))[2:].upper().zfill(2)
-    blueHex = str(hex(int(values["blue"])))[2:].upper().zfill(2)
-    colHex = redHex + greenHex + blueHex
-    pdf = pikepdf.open(values["input"])
-    pdfdye.mangle_pdf(pdf, pdfdye.parse_color(colHex))
-    pdf.save(values["output"])
+while True:
+    event, values = window.read()
+    # See if user wants to quit or window was closed
+    if (event == psg.WINDOW_CLOSED) or (event == "Quit"):
+        break
+    elif (event == "submit"):
+        redHex = str(hex(int(values["red"])))[2:].upper().zfill(2)
+        greenHex = str(hex(int(values["green"])))[2:].upper().zfill(2)
+        blueHex = str(hex(int(values["blue"])))[2:].upper().zfill(2)
+        colHex = redHex + greenHex + blueHex
+        pdf = pikepdf.open(values["input"])
+        pdfdye.mangle_pdf(pdf, pdfdye.parse_color(colHex))
+        pdf.save(values["output"])
+        break
